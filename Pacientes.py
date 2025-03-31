f3b79782f59ef3b35f780a29a38b2bfdf7648c05
Makuin95-afk <varelaoyuelaivanfernando@gmail.com>
2025-03-30 18:15:46 -0600

    Update Pacientes.py

--- Pacientes.py
+++ Pacientes.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -18,7 +18,7 @@ def conexionMySQL():
 
 # Función para ejecutar consultas SQL
 def ejecutarSQL(consulta, parametros=None, recibir_datos=False):
-    conexion = conexionSQLServer()
+    conexion = conexionMySQL()
     if isinstance(conexion, str):
         return conexion  # Si la conexión falló, devolvemos el error
     
